40b3c76c107b4b9c7714674e537cdf2249a30156
Nadiv-Y <nadiv199@gmail.com>
2025-05-31 19:50:06 +0300

    nadiv's javaScript

---
+++ lessons/1lesson/premitiveType.js
@@ -0,0 +1,22 @@
+
+//roels for JS:
+// 1. you cant use in reserves words as variables, for example: if, let, for, while ext.
+//2.give spacie names for variables.
+//3. dont start with numbers.
+//4. JS is caseSensitive
+//5. var must strats with english 
+
+let fullName = 'nadiv yosef'
+
+let age = '26'
+
+console.log(fullName, age)
+
+let workType = 'im work in high tech company'
+
+console.log('hi im', fullName,'im', age, 'years old and', workType)
+
+let a = 1 , b=0
+console.log(a/b)
+
+console.log(typeof workType, a);
---
+++ lessons/1lesson/referenceType.js
@@ -0,0 +1,36 @@
+const person = {
+    firstName: 'Nadiv',
+    lastName: 'Yosef',
+    age: 26
+}
+
+console.log(person);
+
+person.id = '206392698'
+// use .--- for strings / number's
+
+console.log(person);
+
+const person1 = person
+
+person1.adress = 'TLV'
+
+console.log(person);
+
+let g = 'gender'
+
+person[g] = 'male'
+
+// use [] for values
+
+console.log(person);
+
+let m = 'merrid'
+
+person.m = true
+
+person[m] = 'yes'
+
+console.log(person1);
+
+
