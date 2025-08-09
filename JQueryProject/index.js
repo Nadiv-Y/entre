543c986fbfe34ed114070aca277963012ebd5ebc
Nadiv-Y <nadiv199@gmail.com>
2025-08-09 12:17:00 +0300

    display all the coins inside card's in the main div + create about modal

--- JQueryProject/index.js
+++ JQueryProject/index.js
@@ -1,32 +1,41 @@
-let cards = document.querySelector(".main");
-//1. create XMLHttpRequest object
-let req = new XMLHttpRequest();
+const cards = document.querySelector(".main");
 
-//2. create the request
-req.open("GET", "https://api.coingecko.com/api/v3/coins/list");
+async function getData() {
+  const response = await fetch("https://api.coingecko.com/api/v3/coins/list");
+  const data = await response.json();
+  const first100Coins = data.slice(0, 100);
+  display100Coins(first100Coins);
+  return first100Coins;
+}
 
-//3.send request
-req.send();
+getData()
+  .then((data) => console.log({ data }))
+  .catch((err) => console.log("error" + err.message));
 
-req.addEventListener("load", function () {
-  //convert the JSON string to JS object
-  let coinsData = JSON.parse(req.responseText);
-  console.log(coinsData.slice(0, 100));
-  let newCoinsData = coinsData.slice(0, 100);
-  for (let i = 0; i < newCoinsData.length; i++) {
-    const coinContainer = `<div id="${newCoinsData[i].id}" class="card mb-3" style="width: 18rem">
+const searchInput = document.querySelector("#search");
+
+function display100Coins(first100Coins) {
+  if (searchInput.value === "") {
+    for (let i = 0; i < first100Coins.length; i++) {
+      const coinContainer = `<div id="${first100Coins[i].id}" class="card mb-3" style="width: 18rem">
                 <div class="card-body">
                   <label class="switch">
                     <input type="checkbox" />
                     <span class="slider"></span>
                   </label>
-                  <h5 class="card-title">${newCoinsData[i].name}</h5>
-                  <p class="card-text">${newCoinsData[i].symbol}</p>
-                  <a href="#" id="${newCoinsData[i].id}" class="btn btn-primary">More Info</a>
+                  <h5 class="card-title">${first100Coins[i].name}</h5>
+                  <p class="card-text">${first100Coins[i].symbol}</p>
+                  <a href="#" id="${first100Coins[i].id}" class="btn btn-primary">More Info</a>
                 </div>
               </div>`;
-    cards.innerHTML += coinContainer;
+      cards.innerHTML += coinContainer;
+    }
   }
-});
+}
 
+document.querySelector("#about").addEventListener("click", modal);
 
+function modal() {
+  const modal = new bootstrap.Modal(document.getElementById("aboutModal"));
+  modal.show();
+}
