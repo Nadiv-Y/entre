a508b210dac64090ba0189f73d2b5d09a84f1899
Nadiv-Y <nadiv199@gmail.com>
2025-09-04 21:41:31 +0300

    the task is done

--- JQueryTask/task.js
+++ JQueryTask/task.js
@@ -6,7 +6,7 @@ async function getCountriesData() {
       "https://restcountries.com/v3.1/all?fields=name,flags,population,region,currencies"
     );
     const data = await response.json();
-    countries.push(...data);
+    countries = data;
     console.log({ countries });
     return countries;
   } catch (err) {
@@ -18,20 +18,26 @@ getCountriesData();
 
 const searchInput = document.querySelector("#search");
 searchInput.addEventListener("input", () => {
-  dislpayCountries(countries);
+  dislplayCountries(countries);
 });
 
-function dislpayCountries(countries) {
+function dislplayCountries(countries) {
   searchTerm = searchInput.value.toLocaleLowerCase();
   const tableBody = document.querySelector("#tbody");
   const totalPop = document.querySelector("#totalPop");
   const averagePop = document.querySelector("#averagePop");
+  const regionTable = document.querySelector("#secondery");
 
   tableBody.innerHTML = "";
 
   if (!searchTerm) {
     totalPop.innerHTML = `<a class="nav-link active" aria-current="page" href="#">Total Countries Population:</a>`;
     averagePop.innerHTML = `<a class="nav-link active" aria-current="page" href="#">Average Population: </a>`;
+    regionTable.innerHTML = `
+        <th scope="row"></th>
+        <td></td>
+        <td></td>
+      `;
     return;
   }
 
@@ -40,11 +46,13 @@ function dislpayCountries(countries) {
   for (let i = 0; i < countries.length; i++) {
     const countryName = countries[i].name.common.toLocaleLowerCase();
     const citizensNumber = countries[i].population;
+    const regions = countries[i].region;
     if (countryName.includes(searchTerm)) {
       filtered.push({
         index: i,
         name: countries[i].name.common,
         population: citizensNumber,
+        region: regions,
       });
     }
   }
@@ -55,24 +63,50 @@ function dislpayCountries(countries) {
 
   const sum = filtered.reduce((total, num) => total + num.population, 0);
   const numberOfCountries = filtered.length;
-  const average = sum / numberOfCountries
- console.log(sum);
- console.log(numberOfCountries);
- 
- console.log(average);
- 
- 
+  const average = sum / numberOfCountries;
 
   totalPop.innerHTML = `<a class="nav-link active" aria-current="page" href="#">Total Countries Population: ${sum.toLocaleString()}</a>`;
   averagePop.innerHTML = `<a class="nav-link active" aria-current="page" href="#">Average Population: ${average.toLocaleString()}</a>`;
 
+  let index = 1;
+
+
   for (let i = 0; i < filtered.length; i++) {
     const row = document.createElement("tr");
     row.innerHTML = ` 
-          <th scope="row">${filtered[i].index}</th>
+          <th scope="row">${index}</th>
           <td>${filtered[i].name}</td>
-          <td>${filtered[i].population}</td>
+          <td>${filtered[i].population.toLocaleString()}</td>
         `;
+    index++;
     tableBody.appendChild(row);
+  }
+
+
+
+  const countRegion = {};
+
+  for (let i = 0; i < filtered.length; i++) {
+    const region = filtered[i].region;
+    countRegion[region] = (countRegion[region] || 0) + 1;
+  }
+
+  displayRegions(countRegion);
+}
+
+function displayRegions(countRegion) {
+  const regionTable = document.querySelector("#secondery");
+  regionTable.innerHTML = "";
+  let index = 1;
+
+  for (const key in countRegion) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <th scope="row">${index}</th>
+        <td>${key}</td>
+        <td>${countRegion[key]}</td>
+      `;
+    regionTable.appendChild(row);
+    index++;
   }
 }
