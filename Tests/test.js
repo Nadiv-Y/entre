38bd4748b4c571208ef18289c85615bc3885c963
Harel <harelasher1@gmail.com>
2025-10-08 09:14:16 +0300

    third word

--- Tests/test.js
+++ Tests/test.js
@@ -1,9 +1,25 @@
-console.log(a); // ?
-// let a = 10;
+//  Implement `once(fn, ctx?)` which returns a version that can run
+// only once and invokes `fn` with `this = ctx` if provided,
+// otherwise preserves the caller’s `this`:
 
+const log = function (a, b) {
+  console.log(this.prefix, a + b)
+}
 
-// function f() {
-// console.log(b); // ?
-// const b = 5;
-// }
-// f();
+const once = function (fn, ctx = this) {
+  let a = true
+  
+  fn = fn.bind(ctx)
+  if (a) {
+    return fn
+    a = false
+  }
+}
+
+const onceLog = once(log, { prefix: "[sum]" })
+
+console.log(onceLog)
+
+onceLog(1, 2) // prints once
+
+onceLog(3, 4) // does not print
