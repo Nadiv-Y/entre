299ef7d16689c857567cc19167abfa786c5a235d
Aslam <aslamjijine1@gmail.com>
2026-02-09 13:12:43 +0200

    save

--- Project 3/Vacation-27880-4578-03/backend/src/2-utils/promote_admin.ts
+++ Project 3/Vacation-27880-4578-03/backend/src/2-utils/promote_admin.ts
@@ -2,11 +2,11 @@ import dal from "./dal";
 
 async function promote() {
     try {
-        console.log("Promoting testuser1 to admin...");
-        const sql = "UPDATE users SET role = 'admin' WHERE username = 'testuser1'";
+        console.log("Promoting admin_test to admin...");
+        const sql = "UPDATE users SET role = 'admin' WHERE username = 'admin_test'";
         const result = await dal.execute(sql);
         console.log("Result:", result);
-        console.log("Promoted testuser1 to admin successfully.");
+        console.log("Promoted admin_test to admin successfully.");
     } catch (err: any) {
         console.error("Error promoting user:", err.message);
     }
